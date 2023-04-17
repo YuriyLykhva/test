342608d57428953f21c180f7212a8275f9aa794b
Yurii_Lykhva <yuriy.lykhva@gmail.com>
2023-04-17 16:19:41 +0300

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello world! " + i);
+            System.out.println("Hello world!     " + i * 10);
         }
-
     }
 }
